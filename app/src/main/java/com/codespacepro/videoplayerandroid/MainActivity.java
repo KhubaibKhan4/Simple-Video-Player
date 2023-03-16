c8b0a282c336481e695fabe9c721e773c12249d4
KhubaibKhan4 <18.bscs.803@gmail.com>
2023-03-16 23:44:49 +0500

    Simple Video Player Android

---
+++ app/src/main/java/com/codespacepro/videoplayerandroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.codespacepro.videoplayerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.videoview);
+
+        String videoPath = "android.resource://" + getPackageName() + "/raw/android_wall";
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+}
